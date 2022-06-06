4a53e853ec7479d706ca1d25986bd33cbf760604
hamzajrifi <hamza.jrifi@gmail.com>
2022-06-06 15:43:43 +0100

    delete expanding

--- built_func/echo.c
+++ built_func/echo.c
@@ -24,66 +24,9 @@ void    ft_echo(t_shell *mini, char **str, int fd)
     a = 1;
     while (str[i])
     {
-        if (strcmp(str[i], "-n") != 0)
-        {
-            if (strcmp(str[i], "$") == 0)
-                ft_putstr_fd(str[i], fd);
-            else if (strcmp(str[i], "$?") == 0)
-            {
-                ft_putstr_fd(ft_itoa(status_exec_g), fd);
-                status_exec_g = 0;
-            }
-            else if (str[i][0] == '$')
-            {
-                while (mini->tab_save_env[j])
-                {
-                    temp = ft_substr(mini->tab_save_env[j], 0, len(mini->tab_save_env[j]));
-                    if (strcmp(temp, ft_strchr(str[i], '$')) == 0)
-                    {
-                        ft_putstr_fd(ft_strchr(mini->tab_save_env[j], '='), fd);
-                        break ;
-                    }
-                    j++;
-                }
-                if (mini->tab_save_exp)
-                {
-                    j = 0;
-                    while (mini->tab_save_exp[j])
-                    {
-                        temp = ft_substr(mini->tab_save_exp[j], 0, len(mini->tab_save_exp[j]));
-                        if (strcmp(temp, ft_strchr(str[i], '$')) == 0)
-                        {
-                            ft_putstr_fd(ft_strchr(mini->tab_save_exp[j], '='), fd);
-                            break ;
-                        }
-                        j++;
-                    }
-                }
-                j = 0;
-            }
-            else if (str[i][1] == 'n' && str[i][2] == 'n')
-            {
-                while (str[i][a])
-                {
-                    if (str[i][a] == 'n')
-                        a++;
-                    else
-                    {
-                        a = 1000;
-                        ft_putstr_fd(str[i], fd);
-                        break;
-                    }
-                }
-            }
-            else if (strcmp(str[i], "~") == 0)
-                search_path_in_env(mini, 2);
-            else
-            {
-                ft_putstr_fd(str[i], fd);
-                if (str[i + 1] != NULL)
-                    write(fd, " ", 1);
-            }
-        }
+        write (fd, str[i], ft_strlen(str[i]));
+        if (str[i + 1] != NULL)
+            write(fd, " ", 1);
         i++;
     }
     if (strcmp(str[1], "-n") != 0)
--- main.c
+++ main.c
@@ -55,29 +55,33 @@ void    ft_mini(t_shell *mini, char *src)
     int     i;
     lst = ft_parser(src);
     
-    i = 0;
+    
     head = lst;
     while (lst)
     {
-        printf("%s -- %s\n" , lst->val[0] , lst->val[1]);
-        lst = lst->next;
+        i = 1;
+        printf("%s ---\n" , lst->val[0]);
+        while (lst->val[i])
+            printf("%s | " , lst->val[i++]);
+        printf("\n");
+            lst = lst->next;
     }
-    lst = head;
-    if (!lst)
-        return;
-    else if (finder_red(lst) == 1)
-    {
-        puts("hana");
-        pipes(mini, lst);
-    }
+    // lst = head;
+    // if (!lst)
+    //     return;
+    // else if (finder_red(lst) == 1)
+    // {
+    //     // puts("hana");
+    //     pipes(mini, lst);
+    // }
     // else if (finder_red(lst) == 2)
     //    ft_redirection(mini, lst, 0);
     // else if (finder_red(lst) == 4)
     //     check_herd(mini, lst);
     // else if (finder_red(lst) == 3)
     //     ft_redin(mini, lst);
-    else 
-       ft_check_built(mini, lst, 1);
+    // else 
+    //    ft_check_built(mini, lst, 1);
 }
 
 int main(int ac, char **av, char **env)
